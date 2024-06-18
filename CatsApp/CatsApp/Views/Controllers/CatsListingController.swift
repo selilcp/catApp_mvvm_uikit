9a6c3b4aed63ddd362ec1878732a25e5e640d771
Selil <selilmcp@gmail.com>
2024-06-18 20:01:46 +0100

    design cats listing screen

---
+++ CatsApp/CatsApp/Views/Controllers/CatsListingController.swift
@@ -0,0 +1,21 @@
+//
+//  CatsListingController.swift
+//  CatsApp
+//
+//  Created by Selil on 18/06/2024.
+//
+
+import UIKit
+
+class CatsListingController: UIViewController {
+    
+    @IBOutlet var tableView: UITableView!
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        title = "CatsApp"
+    }
+
+
+}
+
---
+++ CatsApp/CatsApp/Views/TableViewCells/CatsListingTableViewCell.swift
@@ -0,0 +1,17 @@
+//
+//  CatsListingTableViewCell.swift
+//  CatsApp
+//
+//  Created by Selil on 18/06/2024.
+//
+
+import UIKit
+
+class CatsListingTableViewCell: UITableViewCell {
+
+    @IBOutlet weak var catImageView: UIImageView!
+    @IBOutlet weak var titleLabel: UILabel!
+    @IBOutlet weak var originLabel: UILabel!
+    @IBOutlet weak var favButton: UIButton!
+    
+}

52da1c737f109a14f7da7e3ff8fd7d032baf69ea
Selil <selilmcp@gmail.com>
2024-06-18 20:08:54 +0100

    design of cat detail screen

---
+++ CatsApp/CatsApp/Views/Controllers/CatDetailsViewController.swift
@@ -0,0 +1,18 @@
+//
+//  CatDetailsViewController.swift
+//  CatsApp
+//
+//  Created by Selil on 18/06/2024.
+//
+
+import UIKit
+
+class CatDetailsViewController: UIViewController {
+
+    @IBOutlet private weak var catImageView: UIImageView!
+    @IBOutlet private weak var titleLabel: UILabel!
+    @IBOutlet private weak var originLabel: UILabel!
+    @IBOutlet private weak var descriptionText: UITextView!
+    @IBOutlet private weak var favButton: UIButton!
+
+}

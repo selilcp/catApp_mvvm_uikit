aea5ea686db17cf3931f910183df6b57160fada2
Selil <selilmcp@gmail.com>
2024-06-18 19:10:37 +0100

    Create 'AppEnvironment' for base url and api key provide to the app

---
+++ CatsApp/CatsApp/Network/AppEnvironment.swift
@@ -0,0 +1,16 @@
+//
+//  AppEnvironment.swift
+//  CatsApp
+//
+//  Created by Selil on 18/06/2024.
+//
+
+import Foundation
+
+struct AppEnvironment {
+    
+    static let development = AppEnvironment()
+    
+    var baseURL:String { "https://api.thecatapi.com/v1/" }
+    var xApiKey:String { "live_diCQpe5cQkq1TMZccyNdpCDVkrNaYcKUOi0cwZbAfIeR8QRQo5fN68qiPLwL6IdJ" }
+}
